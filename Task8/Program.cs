f274a74a3fba844dec4264fe8ec67bd596897c20
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2022-11-26 15:11:10 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Требуется ввести число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > -2 && N < 2)
+{
+    Console.WriteLine($"Нет четных чисел в диапазоне от 1 до {N}");
+}
+else  if (N > 0)
+    {
+        int i = 2;
+        while (i <= N)
+        {
+            Console.WriteLine(i);
+            i=i+2;
+        }
+    }
+    else 
+    {
+        int rem = N % 2;
+        if (rem == 0)
+        { 
+            while (N < 0)
+            {
+                Console.WriteLine(N);
+                N = N + 2;
+            }
+        }
+        else 
+        {
+            N= N + 1;
+            while (N < 0)
+            {
+                Console.WriteLine(N);
+                N = N + 2;
+            }
+        }
+    }
